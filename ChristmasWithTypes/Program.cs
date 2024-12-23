2491dd9c387d9f58de91ff0346c94ff3c5974b01
Vannye Lima <vannye_l@yahoo.com>
2024-12-23 16:24:00 -0500

    exercise assignment

--- ChristmasWithTypes/Program.cs
+++ ChristmasWithTypes/Program.cs
@@ -14,7 +14,7 @@ namespace ChristmasWithTypes
             xmas.Santa = "Kris Kringle";
 
             //TODO Insert 3 presents you would like for xmas.  They must be strings
-            xmas.Presents =  new string[3]{"Running Shoes", "Dog", "Guitar" }; 
+            xmas.Presents =  new string[4]{"Running Shoes", "Dog", "Guitar", "Slushie Machine" }; 
 
             //TODO Set the TreeHeight to 10
             xmas.TreeHeight = 10;
